0253eb9f7879a03cd5877fdabf58466512617504
sametkaradag <sametkaradag@gmail.com>
2024-08-28 13:57:26 +0200

    Added the service account

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,3 +62,38 @@ resource "google_data_catalog_tag_template" "bigquery_column_descriptions" {
   }
   force_delete = true
 }
+
+
+# Create Service Account
+resource "google_service_account" "dataplex_aspect_account" {
+  account_id   = "dataplex-sa"
+  display_name = "Dataplex  Service Account"
+  disabled     = false
+}
+
+
+# Bind Role to Service Account
+resource "google_project_iam_member" "dataplex_aspect_binding" {
+  project = var.project_id
+  role    = "roles/datacatalog.tagTemplateUser"
+  member  = "serviceAccount:${google_service_account.dataplex_aspect_account.email}"
+}
+
+# Grant service account access to Dataplex API
+resource "google_project_iam_member" "dataplex_aspect_access" {
+  project = var.project_id
+  role    = "roles/dataplex.viewer"
+  member  = "serviceAccount:${google_service_account.dataplex_aspect_account.email}"
+}
+
+# Grant service account access to Data Lineage API
+resource "google_project_iam_member" "data_lineage_aspect_access" {
+  project = var.project_id
+  role    = "roles/datalineage.viewer"
+  member  = "serviceAccount:${google_service_account.dataplex_aspect_account.email}"
+}
+
+# Output Service Account Credentials
+output "service_account_email" {
+  value = google_service_account.dataplex_aspect_account.email
+}
